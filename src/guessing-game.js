4d7e2ccdabd3a8ee0afb0c7c5fcf23c805e6bd73
SAKURALIX <sakura.xlix@gmail.com>
2023-02-27 23:58:16 +0300

    feat: initial code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,29 +1,67 @@
 class GuessingGame {
     constructor() {}
 
-    setRange(min, max) {
-        let rangeArray = [];
-    	let range = max - min;
-    	for (i = 0; i <= range; i++) {
-    		rangeArray.push(i);
+    class Node {
+    	constructor(value) {
+    		this.value = value;
+    		this.left = null;
+    		this.right = null;
     	}
-    	console.log(rangeArray);
-        return rangeArray 
-
-
     }
 
-    guess() {
+class GuessingGame {
+    constructor() {
+    	this.root = null;
+    	this.range = 0;
+    }
+    
+    
 
+    setRange(min, max) {
+    	let minimum = min;
+    	let maximum = max;
+    	let rangeValue = maximum - minimum;
+     this.range = rangeValue;
+     console.log(this.range);
+     return rangeValue;
+    }
+    
+    
+
+    guess() {
+    	 if (!this.root) {
+   // 	 let range = setRange(min, max);
+    	 let candidate = Math.round(this.range/ 2);
+    	 this.root = new Node();
+    	 this.root.value = candidate;
+    	 console.log(this.root.value);
+    	 return candidate;
+     	}
     }
 
     lower() {
-
+    	this.right = game.guess()
+    	min = min;
+    	max = this.root.value;
+    	this.range = max - min;
+    	current.left = newNode;
     }
 
     greater() {
+    	let greater = guess()
 
     }
+}
+
+function setRange(min, max) {
+	   game.setRange(min, max);
+	   game.guess();
+}
+
+let game = new GuessingGame();
+
+setRange(0, 10);
+
 }
 
 module.exports = GuessingGame;
