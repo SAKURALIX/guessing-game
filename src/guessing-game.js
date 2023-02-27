888d808183c95de0f126a8ced48ff92158a0d471
SAKURALIX <sakura.xlix@gmail.com>
2023-02-28 02:59:07 +0300

    feat: add some code not ready yet

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,67 +1,98 @@
-class GuessingGame {
-    constructor() {}
-
-    class Node {
-    	constructor(value) {
-    		this.value = value;
-    		this.left = null;
-    		this.right = null;
-    	}
+class Node {
+    constructor(value) {
+    	this.value = value;
+    	this.left = null;
+    	this.right = null;
     }
+}
 
 class GuessingGame {
     constructor() {
     	this.root = null;
     	this.range = 0;
+        this.minimum = null;
+        this.current = null;
     }
     
-    
-
     setRange(min, max) {
-    	let minimum = min;
+    	this.minimum = min;
+        let minimum = min;
     	let maximum = max;
     	let rangeValue = maximum - minimum;
-     this.range = rangeValue;
-     console.log(this.range);
-     return rangeValue;
+        this.range = rangeValue;
+        // console.log(this.range);
+        return rangeValue;
     }
     
     
 
     guess() {
-    	 if (!this.root) {
-   // 	 let range = setRange(min, max);
+    	 if (this.root === null) {
     	 let candidate = Math.round(this.range/ 2);
     	 this.root = new Node();
+         this.current = this.root;
     	 this.root.value = candidate;
-    	 console.log(this.root.value);
+    	//  console.log(this.current);
     	 return candidate;
-     	}
+     	} else if (this.root != null) {
+            let candidate = Math.round((this.minimum + this.range)/ 2);
+            this.current = new Node();
+            this.current.value = candidate;
+            this.root.left = this.current;
+
+            // console.log(this.root);
+            console.log(this.current);
+
+            return this.current;
+        }
     }
 
     lower() {
-    	this.right = game.guess()
-    	min = min;
-    	max = this.root.value;
+    	// this.right = game.guess()
+        if (this.current === this.root) {
+    	let min = this.minimum;
+    	let max = this.root.value;
     	this.range = max - min;
-    	current.left = newNode;
+        // console.log(this.range )
+
+        } else if (this.current != this.root) {
+            this.root.left = this.current;
+            let min = this.minimum;
+            let max = this.current.value;
+            this.range = max - min;
+            // console.log(min)
+            // console.log(max)   
+            // console.log(this.range)
+        }
+
+    	// current.left = newNode;
     }
-
+// здесь остановился, возможно неправильно
     greater() {
-    	let greater = guess()
-
+        this.root.right = this.current;
+        let min = this.current.value;
+        let max = this.root.value;
+        this.range = max - min;
     }
 }
 
 function setRange(min, max) {
 	   game.setRange(min, max);
 	   game.guess();
+       game.lower();
+       game.guess();
+       game.lower();
+       game.guess();
+       game.lower();
+       game.guess();
+       game.greater();
+       game.guess();    
 }
 
 let game = new GuessingGame();
 
-setRange(0, 10);
+setRange(0, 4048);
+    
 
-}
 
-module.exports = GuessingGame;
+// module.exports = GuessingGame;
